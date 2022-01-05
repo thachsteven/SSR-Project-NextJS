9fdfed8acb61418b8440ef983efe433d798be9b0
Nguyen Ngoc Thach <thachsteven.marketing@gmail.com>
2022-01-05 17:46:25 +0700

    optimize data fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -3,7 +3,7 @@ import EventContent from "../../components/event-detail/event-content";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventSummary from "../../components/event-detail/event-summary";
 import ErrorAlert from "../../components/events/error-alert";
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
 const EventDetailPage = ({selectedEvent}) => {
   const event = selectedEvent
@@ -12,9 +12,9 @@ const EventDetailPage = ({selectedEvent}) => {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -41,18 +41,19 @@ export async function getStaticProps(context) {
   return {
     props: {
       selectedEvent: event
-    }
+    },
+    revalidate: 30
   }
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
 
   const paths = events.map(event => ({params: {eventId: event.id}}))
 
   return {
     paths: paths,
-    fallback: false //if fallback is false, with the unknown id page will redirect to 404 page 
+    fallback: true //if fallback is false, with the unknown id page will redirect to 404 page 
   }
 }
 
